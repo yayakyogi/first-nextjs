f9f2846586c0d1f9c5f2762a86d94ee3bdc6a3cd
yayakyogi <yayaktaka@gmail.com>
2022-01-01 13:49:35 +0700

    add routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
+import Link from "next/link";
+
 export default function home() {
   return (
-    <div>
-      <h1>Blog</h1>
-    </div>
+    <>
+      <header>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog">
+            <a>Blog</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/user">
+            <a>User</a>
+          </Link>
+        </li>
+      </header>
+      <div>
+        <h1>Blog</h1>
+      </div>
+    </>
   );
 }
