285913989fed8fa48a8f09bf579b2d59040962f9
yayakyogi <yayaktaka@gmail.com>
2022-01-01 15:27:49 +0700

    add auto redirection to homepage

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Page404() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 2000);
+  }, []);
   return (
     <div>
       <h1 className="title-not-found">Ooops...</h1>
