7af19ab756bef3329399f382c9e1e9edee573e2c
yayakyogi <yayaktaka@gmail.com>
2022-01-02 05:50:44 +0700

    try getStaticProps

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,9 +1,43 @@
 import Layout from "../../components/layout";
-
-export default function index() {
+// import { GetStaticProps } from "next";
+interface UserProps {
+  dataUsers: Array<any>;
+}
+export default function index(props: UserProps) {
+  const { dataUsers } = props;
+  console.log(dataUsers);
   return (
     <Layout pageTitle="User Page">
-      <h1>User Page</h1>
+      {dataUsers.map((users, key) => {
+        return (
+          <div key={key}>
+            <p>{users.name}</p>
+            <p>{users.email}</p>
+          </div>
+        );
+      })}
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const dataUsers = await res.json();
+
+  return {
+    props: {
+      dataUsers,
+    },
+  };
+}
+
+// * Call function getStaticProps when from next
+// export const getStaticProps: GetStaticProps = async () => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/users");
+//   const dataUsers = await res.json();
+//   return {
+//     props: {
+//       dataUsers,
+//     },
+//   };
+// };
