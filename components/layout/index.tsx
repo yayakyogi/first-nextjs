41a20a4303eb7fbc696b71fa39d1a445f6206953
yayakyogi <yayaktaka@gmail.com>
2022-01-01 15:39:17 +0700

    add meta data

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -2,17 +2,25 @@ import { ReactNode } from "react";
 import Header from "../header";
 import Footer from "../footer";
 import styles from "./Layout.module.css";
+import Head from "next/head";
 
 interface LayoutProps {
   children: ReactNode;
+  pageTitle: string;
 }
 export default function Layout(props: LayoutProps) {
-  const { children } = props;
+  const { children, pageTitle } = props;
   return (
-    <div className={styles.container}>
-      <Header />
-      <div className={styles.content}>{children}</div>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>First NextJS | {pageTitle}</title>
+        <meta name="description" content="First website using Next JS" />
+      </Head>
+      <div className={styles.container}>
+        <Header />
+        <div className={styles.content}>{children}</div>
+        <Footer />
+      </div>
+    </>
   );
 }
