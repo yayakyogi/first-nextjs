7ab1b4877da392bf82da02cde337b0353ff95e5a
yayakyogi <yayaktaka@gmail.com>
2022-01-01 16:58:38 +0700

    try image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Layout from "../components/layout";
 import styles from "../styles/Home.module.css";
 
@@ -5,6 +6,7 @@ export default function home() {
   return (
     <Layout pageTitle="Home Page">
       <h1 className={styles.title}>Ginantaka Code</h1>
+      <Image src="/image-1.jpg" width={200} height={200} alt="image-1" />
     </Layout>
   );
 }
