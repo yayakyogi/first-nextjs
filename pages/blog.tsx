44d91e85c333cdcf0b0734f80edbf0aa1d241ae6
yayakyogi <yayaktaka@gmail.com>
2022-01-02 08:40:51 +0700

    try getStaticServerSide

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,9 +1,38 @@
 import Layout from "../components/layout";
 
-export default function blog() {
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+interface BlogProps {
+  dataBlog: Post[];
+}
+export default function blog(props: BlogProps) {
+  const { dataBlog } = props;
+  console.log(dataBlog);
+
   return (
     <Layout pageTitle="Blog Page">
-      <h1>Blog</h1>
+      {dataBlog.map((blog) => {
+        return (
+          <div key={blog.id}>
+            <h1>{blog.title}</h1>
+            <p>{blog.body}</p>
+          </div>
+        );
+      })}
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const dataBlog = await res.json();
+
+  return {
+    props: {
+      dataBlog,
+    },
+  };
 }
